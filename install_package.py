814180c097e5d0b634f1f2c96fb2349add47aab1
mjshakir <maslove23@gmail.com>
2023-07-20 17:59:59 -0300

    Change: install from requirement

--- install_package.py
+++ install_package.py
@@ -1,36 +1,16 @@
-import platform
 import subprocess
-import sys
 
-# List of required Python packages
-REQUIRED_PACKAGES = [
-    "numpy",
-    "tabulate",
-    "pyyaml",
-]
-
-def install_package(package_name):
-    """
-    Install the specified Python package using pip.
-
-    Args:
-        package_name (str): Name of the Python package to install.
-    """
-    print(f"Installing {package_name}...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
-
-def main():
-    # Get the current platform
-    current_platform = platform.system()
-
-    # Check for missing dependencies and install them if needed
-    for package in REQUIRED_PACKAGES:
-        try:
-            __import__(package)
-        except ImportError:
-            install_package(package)
-
-    print("All dependencies are installed and ready to use.")
+def install_dependencies():
+    try:
+        # Read the requirements.txt file
+        with open('requirements.txt', 'r') as file:
+            requirements = file.read().splitlines()
+        
+        # Install the dependencies using pip
+        subprocess.check_call(['pip', 'install', *requirements])
+        print("Dependencies installed successfully.")
+    except Exception as e:
+        print(f"Error installing dependencies: {e}")
 
 if __name__ == "__main__":
-    main()
+    install_dependencies()
