b677369645522a680d0bad484fe7b40223d1fa46
mjshakir <maslove23@gmail.com>
2023-07-20 14:16:01 -0300

    Change: mange yaml and csv

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import argparse
-from ranked_voting import RankedVoting
-from typing import Dict, Tuple, Optional
+from pathlib import Path
+import RankedVotingFromCSV, RankedVotingFromYAML
 
-def main() -> None:
-    parser = argparse.ArgumentParser(description='Ranked Voting')
-    parser.add_argument('csv_file', help='Path to the CSV file (without the .csv extension) containing the ranked voting data')
-    parser.add_argument('--show-intermediate', action='store_true', help='Show intermediate results after each round of vote redistribution')
-    args = parser.parse_args()
+def main(args: argparse.Namespace) -> None:
+    candidates_file = args.candidates
+    voter_files = args.voters
 
-    filename: str = args.csv_file
-    if not filename.endswith('.csv'):  # Check if the user provided .csv extension; if not, raise an error
-        print("Error: The file must be in .csv format.")
-        exit(1)
+    if candidates_file.suffix == ".csv" and all(file.suffix == ".csv" for file in voter_files):
+        voting_system = RankedVotingFromCSV(candidates_file, voter_files, show_intermediate=args.intermediate)
+    elif candidates_file.suffix == ".yaml" and all(file.suffix == ".yaml" for file in voter_files):
+        voting_system = RankedVotingFromYAML(candidates_file, voter_files, show_intermediate=args.intermediate)
+    else:
+        print("Invalid file format. Candidates and voter files must be either all CSV or all YAML.")
+        return
 
-    show_intermediate: bool = args.show_intermediate
-
-    ranked_voting = RankedVoting(filename, show_intermediate)
-    percentages, _ = ranked_voting.run_ranked_voting()
-
-    if show_intermediate:
-        ranked_voting.display_intermediate_results(percentages)
-
-    ranked_voting.display_final_results(percentages)
-
-    # If you don't need the percentages and winner in the main script,
-    # you can ignore the return values as follows:
-    # ranked_voting.run_ranked_voting()
+    voting_system.run_ranked_voting()
+    voting_system.display_final_results(voting_system.vote_percentages)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Ranked Voting System")
+    parser.add_argument("candidates", type=Path, help="Path to the candidates file (CSV or YAML)")
+    parser.add_argument("voters", type=Path, nargs="+", help="Paths to the voter files (CSV or YAML)")
+    parser.add_argument("--intermediate", action="store_true", help="Display intermediate results")
+    args = parser.parse_args()
+    main(args)
