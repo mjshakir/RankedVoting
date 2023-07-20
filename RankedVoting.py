3ad4a0cb044dad4e9caf1d5d160755f6abc68c66
mjshakir <maslove23@gmail.com>
2023-07-20 14:16:49 -0300

    Change: integrate numpy and redesign to be based

--- RankedVoting.py
+++ RankedVoting.py
@@ -1,117 +1,74 @@
-import csv
-from typing import Dict, Tuple
-from tabulate import tabulate
+
+import numpy as np
+from typing import Dict
 from collections import defaultdict
+from tabulate import tabulate
 
 class RankedVoting:
-    """
-    A class to perform Ranked Voting calculations based on a CSV file.
-
-    Attributes:
-        filename (str): The path to the CSV file containing the ranked voting data.
-        candidates (list[str]): The list of candidate names extracted from the CSV file.
-        votes (list[list[int]]): The list of votes, where each vote is represented as a list of integers.
-        show_intermediate (bool): A flag to determine whether to display intermediate results.
-    """
-    def __init__(self, filename: str, show_intermediate: bool = False):
+    def __init__(self, show_intermediate: bool = False):
         """
-        Initializes the RankedVoting object.
+        Initialize the RankedVoting instance.
 
         Args:
-            filename (str): The path to the CSV file containing the ranked voting data.
-            show_intermediate (bool, optional): Whether to display intermediate results after each round of vote redistribution.
-                                                Defaults to False.
+            show_intermediate (bool, optional): If True, display intermediate results. Defaults to False.
         """
-        self.filename = filename
-        self.candidates = []
-        self.votes = []
         self.show_intermediate = show_intermediate
+        self.candidates = []
+        self.voters = {}
+        self.vote_percentages = {}
+        self.vote_counts = defaultdict(int)
 
-    def _read_csv(self) -> None:
+    def initialize_votes(self) -> None:
         """
-        Reads the CSV file and extracts candidate names and voter preferences.
+        Initialize the votes for each candidate.
         """
-        if not self.filename.endswith('.csv'):
-            raise ValueError("Invalid file format. The file must be in .csv format.")
+        self.vote_counts = defaultdict(int)
 
-        with open(self.filename, 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            self.candidates = next(reader)[1:]  # Extract candidate names from the first row (excluding the first cell)
-
-            voters = set()
-            for row in reader:
-                voter = row[0]
-                if voter in voters:
-                    raise ValueError(f"Duplicate voter found: {voter}")
-                voters.add(voter)
-
-                vote_row = [voter]
-                for vote in row[1:]:
-                    try:
-                        vote = int(vote)
-                        if 0 < vote <= len(self.candidates):
-                            vote_row.append(vote)
-                    except ValueError:
-                        pass
-
-                self.votes.append(vote_row)
-
-    def _count_votes(self) -> Dict[str, int]:
+    def calculate_vote_percentages(self) -> None:
         """
-        Counts the votes received by each candidate.
+        Calculate the percentages of votes received by each candidate using NumPy.
+
+        Note: NumPy optimizations are relevant for large voter numbers.
+        """
+        total_votes = sum(self.vote_counts.values())
+        candidates_arr = np.array(self.candidates)
+        votes_arr = np.array(list(self.vote_counts.values()))
+        self.vote_percentages = dict(zip(candidates_arr, (votes_arr / total_votes) * 100 if total_votes > 0 else 0))
+
+    def is_winner_found(self) -> bool:
+        """
+        Check if a winner has been found (i.e., any candidate has more than 50% of the votes).
 
         Returns:
-            Dict[str, int]: A dictionary mapping each candidate name to the total number of votes received.
+            bool: True if a winner is found, False otherwise.
         """
-        vote_counts = defaultdict(int)
-        for vote_row in self.votes:
-            for _, candidate in enumerate(vote_row[1:], start=1):
-                vote_counts[self.candidates[candidate - 1]] += 1
-        return vote_counts
+        return any(percentage > 50.0 for percentage in self.vote_percentages.values())
 
-    def _calculate_percentages(self, vote_counts: Dict[str, int], total_votes: int) -> Dict[str, float]:
+    def get_least_voted_candidate(self) -> str:
         """
-        Calculates the percentage of votes received by each candidate.
-
-        Args:
-            vote_counts (Dict[str, int]): A dictionary mapping each candidate name to the total number of votes received.
-            total_votes (int): The total number of votes cast.
+        Get the candidate with the least votes.
 
         Returns:
-            Dict[str, float]: A dictionary mapping each candidate name to the percentage of votes received.
+            str: The name of the candidate with the least votes.
         """
-        percentages = {candidate: (votes / total_votes) * 100 for candidate, votes in vote_counts.items()}
-        return percentages
+        return min(self.vote_counts, key=self.vote_counts.get)
 
-    def _redistribute_votes(self, winner: str, vote_counts: Dict[str, int]) -> None:
+    def redistribute_votes(self, least_voted_candidate: str) -> None:
         """
-        Redistributes votes from non-winning candidates to their next preferences.
-
-        Args:
-            winner (str): The name of the winning candidate.
-            vote_counts (Dict[str, int]): A dictionary mapping each candidate name to the total number of votes received.
-        """
-        for vote_row in self.votes:
-            if vote_row[1] != winner:
-                for j, candidate in enumerate(vote_row[2:], start=2):
-                    if candidate == winner:
-                        vote_row[1] = winner
-                        vote_counts[winner] += 1
-                        break
-
-    def _find_winner(self, percentages: Dict[str, float]) -> str:
-        """
-        Finds the candidate with the highest percentage of votes.
+        Redistribute votes from the candidate with the least votes to the other candidates based on preferences.
 
         Args:
-            percentages (Dict[str, float]): A dictionary mapping each candidate name to the percentage of votes received.
-
-        Returns:
-            str: The name of the winning candidate.
+            least_voted_candidate (str): The name of the candidate with the least votes.
         """
-        return max(percentages, key=percentages.get)
-    
-    def _display_intermediate_results(self, percentages: Dict[str, float]) -> None:
+        for voter, preferences in self.voters.items():
+            least_voted_rank = preferences.get(least_voted_candidate)
+            if least_voted_rank is not None and least_voted_rank in self.candidates:
+                least_voted_candidate = least_voted_rank
+            else:
+                least_voted_candidate = next(iter(preferences))
+            self.vote_counts[least_voted_candidate] += 1
+
+    def display_intermediate_results(self, percentages: Dict[str, float]) -> None:
         """
         Displays intermediate voting results.
 
@@ -124,7 +81,7 @@ class RankedVoting:
         print(tabulate(table_data, headers=headers, tablefmt="grid"))
         print()
 
-    def _display_final_results(self, percentages: Dict[str, float]) -> None:
+    def display_final_results(self, percentages: Dict[str, float]) -> None:
         """
         Displays final voting results and highlights the winning candidate.
 
@@ -139,31 +96,3 @@ class RankedVoting:
             winner_mark = "*" if candidate == winner else ""
             table_data.append([candidate, f"{percentage:.2f}%", winner_mark])
         print(tabulate(table_data, headers=headers, tablefmt="grid"))
-
-    def run_ranked_voting(self) -> Tuple[Dict[str, float], str]:
-        """
-        Performs the ranked voting calculations and returns the percentages of votes received by each candidate
-        and the winning candidate.
-
-        Returns:
-            Tuple[Dict[str, float], str]: A tuple containing a dictionary mapping each candidate name to the percentage
-            of votes received, and the winning candidate.
-        """
-        self.initialize_votes()
-
-        while not self.is_winner_found():
-            self.calculate_vote_percentages()
-            if self.show_intermediate:
-                self.display_intermediate_results(self.vote_percentages)
-
-            least_voted_candidate = self.get_least_voted_candidate()
-            if not least_voted_candidate:
-                break
-
-            self.redistribute_votes(least_voted_candidate)
-
-        self.calculate_vote_percentages()
-        self.display_final_results(self.vote_percentages)
-
-        winner = max(self.vote_percentages, key=self.vote_percentages.get)
-        return self.vote_percentages, winner
