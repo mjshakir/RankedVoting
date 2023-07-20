7ca18233fcf9fa37a8025ed15510849e68ddf828
mjshakir <maslove23@gmail.com>
2023-07-20 15:44:56 -0300

    Change: to make the changed in the files

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import argparse
-from pathlib import Path
-import RankedVotingFromCSV, RankedVotingFromYAML
+import os
 
 def main(args: argparse.Namespace) -> None:
-    candidates_file = args.candidates
-    voter_files = args.voters
+    """
+    Main function to run the Ranked Voting System using CSV or YAML files.
 
-    if candidates_file.suffix == ".csv" and all(file.suffix == ".csv" for file in voter_files):
-        voting_system = RankedVotingFromCSV(candidates_file, voter_files, show_intermediate=args.intermediate)
-    elif candidates_file.suffix == ".yaml" and all(file.suffix == ".yaml" for file in voter_files):
-        voting_system = RankedVotingFromYAML(candidates_file, voter_files, show_intermediate=args.intermediate)
+    Args:
+        args (argparse.Namespace): Command-line arguments.
+
+    Returns:
+        None
+    """
+    main_folder = args.input_folder
+
+    if args.file_format == "csv":
+        from ranked_voting_from_csv import RankedVotingFromCSV
+
+        voting_system = RankedVotingFromCSV(os.path.join(main_folder, args.file_name), show_intermediate=args.intermediate)
+    elif args.file_format == "yaml":
+        from ranked_voting_from_yaml import RankedVotingFromYAML
+
+        voting_system = RankedVotingFromYAML(main_folder, show_intermediate=args.intermediate)
     else:
-        print("Invalid file format. Candidates and voter files must be either all CSV or all YAML.")
-        return
+        raise ValueError("Invalid file format. Please choose 'csv' or 'yaml'.")
 
     voting_system.run_ranked_voting()
     voting_system.display_final_results(voting_system.vote_percentages)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ranked Voting System")
-    parser.add_argument("candidates", type=Path, help="Path to the candidates file (CSV or YAML)")
-    parser.add_argument("voters", type=Path, nargs="+", help="Paths to the voter files (CSV or YAML)")
-    parser.add_argument("--intermediate", action="store_true", help="Display intermediate results")
+    parser.add_argument("input_folder", type=str, help="Path to the main folder containing the input file.")
+    parser.add_argument("file_name", type=str, help="Name of the input file (without the extension).")
+    parser.add_argument("file_format", choices=["csv", "yaml"], help="File format ('csv' or 'yaml').")
+    parser.add_argument("--intermediate", action="store_true", help="Display intermediate results.")
     args = parser.parse_args()
     main(args)
